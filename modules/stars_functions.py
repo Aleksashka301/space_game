9afc3328f16b23cd87b70d34d2136ee7b57ee199
Aleksashka301 <aleksashka301@gmail.com>
2026-01-13 19:46:10 +0500

    Update stars_functions.py

--- modules/stars_functions.py
+++ modules/stars_functions.py
@@ -1,29 +1,24 @@
 import asyncio
 import curses
-import random
 
 
-async def blink(canvas, row, column, symbol='*'):
-    for _ in range(random.randint(1, 31)):
+async def blink(canvas, row, column, offset_tics, symbol='*'):
+    for _ in range(offset_tics):
         await asyncio.sleep(0)
 
     while True:
         canvas.addstr(row, column, symbol, curses.A_DIM)
-        canvas.refresh()
         for _ in range(18):
             await asyncio.sleep(0)
 
         canvas.addstr(row, column, symbol)
-        canvas.refresh()
         for _ in range(2):
             await asyncio.sleep(0)
 
         canvas.addstr(row, column, symbol, curses.A_BOLD)
-        canvas.refresh()
         for _ in range(4):
             await asyncio.sleep(0)
 
         canvas.addstr(row, column, symbol)
-        canvas.refresh()
         for _ in range(2):
             await asyncio.sleep(0)
