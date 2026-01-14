0f96153fc04e476535a4739767f43cf72714efd7
Aleksashka301 <aleksashka301@gmail.com>
2026-01-14 21:13:29 +0500

    Update fire_functions.py

--- modules/fire_functions.py
+++ modules/fire_functions.py
@@ -2,18 +2,28 @@ import asyncio
 import curses
 
 
-async def fire(canvas, start_row, start_column, rows_speed=-0.4, columns_speed=0):
+async def fire(canvas, start_row, start_column, tics_per_sec, rows_speed=-0.4, columns_speed=0):
+    FLASH_SECONDS = 0.2
+    CORE_SECONDS = 1.6
+    DISAPPEAR_SECONDS = 0.2
+    FLIGHT_STEP_SECONDS = 0.2
+
+    flash_tics = int(FLASH_SECONDS * tics_per_sec)
+    core_tics = int(CORE_SECONDS * tics_per_sec)
+    disappear_tics = int(DISAPPEAR_SECONDS * tics_per_sec)
+    flight_step_tics = int(FLIGHT_STEP_SECONDS * tics_per_sec)
+
     row, column = start_row, start_column
     canvas.addstr(round(row), round(column), '*')
-    for _ in range(2):
+    for _ in range(flash_tics):
         await asyncio.sleep(0)
 
     canvas.addstr(round(row), round(column), 'O')
-    for _ in range(16):
+    for _ in range(core_tics):
         await asyncio.sleep(0)
 
     canvas.addstr(round(row), round(column), ' ')
-    for _ in range(2):
+    for _ in range(disappear_tics):
         await asyncio.sleep(0)
 
     row += rows_speed
@@ -25,7 +35,7 @@ async def fire(canvas, start_row, start_column, rows_speed=-0.4, columns_speed=0
 
     while 0 < row < max_row and 0 < column < max_column:
         canvas.addstr(round(row), round(column), symbol)
-        for _ in range(2):
+        for _ in range(flight_step_tics):
             await asyncio.sleep(0)
 
         canvas.addstr(round(row), round(column), ' ')
